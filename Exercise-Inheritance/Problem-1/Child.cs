5af8c7ccc9b61f60859117eb3f6659eecfecc79b
hoanglongle77 <longlnhgcd201639@fpt.edu.vn>
2023-02-02 14:53:06 +0700

    Add project files.

---
+++ Exercise-Inheritance/Problem-1/Child.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Inheritance.Problem_1
+{
+    internal class Child : Person
+    {
+        public Child(string name, int age) : base(name, age)
+        {
+        }
+    }
+}
---
+++ Exercise-Inheritance/Problem-1/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Inheritance.Problem_1
+{
+    internal class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name { get => name; set => name=value; }
+        public int Age { get => age; set => age=value; }
+
+
+        public Person()
+        {
+        }
+
+        public Person(string name, int age)
+        {
+            Name=name;
+            Age=age;
+        }
+
+        public override string? ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(String.Format("Name: {0}, Age: {1}", this.Name, this.Age));
+            return stringBuilder.ToString();
+        }
+
+    }
+}
